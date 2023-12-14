6faa386a29dd03f2c67a71abba5b5cd08374b3d7
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2023-12-14 16:11:58 -0300

    Contatos adicionado na Component Footer

--- src/app/pages/Footer/components/NavContacts/index.tsx
+++ src/app/pages/Footer/components/NavContacts/index.tsx
@@ -4,7 +4,7 @@ import { MdEmail } from "react-icons/md";
 
 export const NavContacts = () => {
   const sizeIcons: number = 34;
-  const fontCuston: string = "font-bold text-amber-500";
+  const fontCuston: string = "font-bold text-amber-50";
   return (
     <nav className="flex lg:flex justify-center gap-5 lg:justify-between ">
       <Link href={"https://github.com/JoaoG23"}>
@@ -12,7 +12,6 @@ export const NavContacts = () => {
           className={`grid justify-items-center gap-1 rounded-lg p-2 lg:p-0 ${fontCuston} duration-200 hover:scale-125`}
         >
           <FaGithub size={sizeIcons} />
-          <p>GitHub</p>
         </div>
       </Link>
       <Link href={"https://www.linkedin.com/in/joaog123/"}>
@@ -20,7 +19,6 @@ export const NavContacts = () => {
           className={`grid justify-items-center gap-1 rounded-lg p-2 lg:p-0 ${fontCuston} duration-200 hover:scale-125`}
         >
           <FaLinkedin size={sizeIcons} />
-          <p>Linkedin</p>
         </div>
       </Link>
       <Link href={"mailto:jguilhermeempresarial@outlook.com"}>
@@ -28,7 +26,6 @@ export const NavContacts = () => {
           className={`grid justify-items-center gap-1 rounded-lg p-2 lg:p-0 ${fontCuston} duration-200 hover:scale-125`}
         >
           <MdEmail size={sizeIcons} />
-          <p>E-mail</p>
         </div>
       </Link>
     </nav>
